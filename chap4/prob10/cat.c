f7e7ffce50ccd201efc5dfe4a03c7a1b24aee4ba
euneunsol <eunsol1882@gmail.com>
2023-10-20 03:51:23 +0000

    prob10

--- chap4/prob10/cat.c
+++ chap4/prob10/cat.c
@@ -1,21 +1,45 @@
 #include <stdio.h>
+#include <string.h>
+
 int main(int argc, char *argv[])
 {
-   FILE *fp;
-   int c;
-   if (argc < 2) 
-      fp = stdin; 			 
-   else{
-	   for(int i=1; i<4; i++){
-		   fp = fopen(argv[i],"r");
-	   }
-   }
+    if (argc < 2) {
+        return 1;
+    }
 
-   c = getc(fp); 			
-   while (c != EOF) { 		
-      putc(c, stdout); 		
-      c = getc(fp);  
-   }
-   fclose(fp); 
-   return 0;
+    int printLineNumbers = 0;
+    int fileIndex = 1;
+
+    if (argc > 2 && strcmp(argv[1], "-n") == 0) {
+        printLineNumbers = 1;
+        fileIndex++;
+    }
+
+    for (int i = fileIndex; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            continue;
+        }
+
+        int c;
+        int lineNumber = 1;
+        int isLineStart = 1;
+
+        while ((c = getc(fp)) != EOF) {
+            if (isLineStart && printLineNumbers) {
+                printf("%d: ", lineNumber);
+                isLineStart = 0;
+            }
+            putc(c, stdout);
+            if (c == '\n') {
+                lineNumber++;
+                isLineStart = 1;
+            }
+        }
+
+        fclose(fp);
+    }
+
+    return 0;
 }
+
