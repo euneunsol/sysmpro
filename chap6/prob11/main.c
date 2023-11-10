c2e0db6b8f85bd8badba6ed6424d9a4d247190fa
euneunsol <eunsol1882@gmail.com>
2023-11-10 07:13:19 +0000

    chap6

--- chap6/prob11/main.c
+++ chap6/prob11/main.c
@@ -23,19 +23,25 @@ int main(int argc, char **argv) {
 
     if (argc == 1)
         dir = ".";
-    else {
+    else if (argc == 2) {
+        if (argv[1][0] == '-') {
+            dir = ".";
+            option = argv[1][1];
+        } else {
+            dir = argv[1];
+        }
+    } else if (argc >= 3) {
         dir = argv[1];
-        if (argc > 2)
-            option = argv[2][1];
+        option = argv[2][1];
     }
-
+    
     if (access(dir, F_OK) == -1) {
         perror(dir);
         exit(EXIT_FAILURE);
     }
 
     if ((dp = opendir(dir)) == NULL) {
-        perror(dir);
+        perror("Error opening directory");
         exit(EXIT_FAILURE);
     }
 
@@ -49,26 +55,47 @@ int main(int argc, char **argv) {
 
     closedir(dp);
     exit(EXIT_SUCCESS);
-}
+} 
 
 void printStat(char *pathname, char *file, struct stat *st, char option) {
-    printf("%5ld ", st->st_blocks);  // 수정된 부분
+    printf("%5ld ", st->st_blocks);
+
+    if (option == 'i') {
+        printf("%lu ", (unsigned long)st->st_ino);
+    }
+
     printf("%c%s ", type(st->st_mode), perm(st->st_mode));
     printf("%3ld ", st->st_nlink);
-    printf("%s %s ", getpwuid(st->st_uid)->pw_name,
-           getgrgid(st->st_gid)->gr_name);
-    printf("%9ld ", st->st_size);
-    printf("%.12s ", ctime(&st->st_mtime) + 4);
 
-    if (option == 'p') {
+    struct passwd *pwd = getpwuid(st->st_uid);
+    struct group *grp = getgrgid(st->st_gid);
+
+    if (pwd != NULL)
+        printf("%s ", pwd->pw_name);
+    else
+        printf("%d ", st->st_uid);
+
+    if (grp != NULL)
+        printf("%s ", grp->gr_name);
+    else
+        printf("%d ", st->st_gid);
+
+    printf("%9ld ", st->st_size);
+
+    char *time_str = ctime(&st->st_mtime);
+    time_str[strlen(time_str) - 1] = '\0';  
+    printf("%s ", time_str);
+
+    if (option == 'p' || option == 'i') {
         if (S_ISDIR(st->st_mode))
             printf("%s/", file);
         else
             printf("%s", file);
-    } else if (option == 'i') {
-        printf("%lu %s", (unsigned long)st->st_ino, file);
     } else if (option == 'Q') {
-        printf("\"%s\"", file);
+        if (S_ISDIR(st->st_mode))
+            printf("\"%s\"", file);
+        else
+            printf("%s", file);
     } else {
         printf("%s", file);
     }
@@ -76,26 +103,26 @@ void printStat(char *pathname, char *file, struct stat *st, char option) {
     printf("\n");
 }
 
-char type(mode_t mode) {
-    if (S_ISREG(mode))
-        return '-';
-    if (S_ISDIR(mode))
-        return 'd';
-    if (S_ISCHR(mode))
-        return 'c';
-    if (S_ISBLK(mode))
-        return 'b';
-    if (S_ISLNK(mode))
-        return 'l';
-    if (S_ISFIFO(mode))
-        return 'p';
-    if (S_ISSOCK(mode))
-        return 's';
+ char type(mode_t mode) {
+     if (S_ISREG(mode))
+         return '-';
+     if (S_ISDIR(mode))
+         return 'd';
+     if (S_ISCHR(mode))
+         return 'c';
+     if (S_ISBLK(mode))
+         return 'b';
+     if (S_ISLNK(mode))
+         return 'l';
+     if (S_ISFIFO(mode))
+         return 'p';
+     if (S_ISSOCK(mode))
+         return 's';
 
-    return '?';
-}
+     return '?';
+ }
 
-char *perm(mode_t mode) {
+ char *perm(mode_t mode) {
     static char perms[11];
     strcpy(perms, "----------");
 
@@ -107,6 +134,5 @@ char *perm(mode_t mode) {
         if (mode & (S_IXUSR >> i * 3))
             perms[i * 3 + 2] = 'x';
     }
-    return (perms);
-}
-
+     return (perms);
+ }
